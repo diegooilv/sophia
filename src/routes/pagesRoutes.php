c362806da98d5398113f9fbe59ceb8dc5fa0b3bc
Diego <diego.oliv2007@gmail.com>
2026-06-09 21:15:49 -0300

    fix: corrige (e adiciona) rotas de páginas

--- src/routes/pagesRoutes.php
+++ src/routes/pagesRoutes.php
@@ -6,5 +6,11 @@ $router->get('/report/material/{id}', [ReportController::class, 'material']);
 $router->get('/report/post/{id}', [ReportController::class, 'post']);
 $router->get('/signup', [SignupController::class, 'signup']);
 $router->post('/signup', [SignupController::class, 'signupForm']);
+$router->get('/login', [LoginController::class, 'login']);
+$router->post('/login', [LoginController::class, 'loginForm']);
+$router->get('/about', [AboutController::class, 'index']);
+
+$router->get('/ia', [ChatBotController::class, 'index']);
+$router->post('/ia', [ChatBotController::class, 'perguntar']);
 
 return $router;
