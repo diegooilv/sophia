18b0f270279488bf64617452bca40590190407e8
Diego <diego.oliv2007@gmail.com>
2026-06-11 12:54:53 -0300

    docs: adicionar referência de CSRF

---
+++ references/csrf.php
@@ -0,0 +1,6 @@
+<?php
+# https://pt.stackoverflow.com/questions/114663/como-prevenir-o-ataque-csrf-sem-frameworks-php
+# https://dev.to/parzival_computer/csrf-protection-in-php-5g6p
+# https://cheatsheetseries.owasp.org/cheatsheets/Cross-Site_Request_Forgery_Prevention_Cheat_Sheet.html
+
+# Ajuda de IA pra melhor compreensão de conteúdos em inglês. GPT-5.5 (OpenAI)
